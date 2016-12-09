eafecc6067d46f890d4205ac0c12d2e2b2c13cf2
Adrian Fish <adrian.r.fish@gmail.com>
2016-12-09 14:21:26 +0000

    New package structure

--- pushdata.py
+++ pushdata.py
@@ -33,6 +33,9 @@ p = re.compile(r"^(\w*)-(\w*)-(\d*)$")
 ckan = ckanapi.RemoteCKAN('http://127.0.0.1:8080', apikey=apikey)
 
 datapath  = path + '/2016'
+
+currentpackages = ckan.action.package_list()
+
 for filename in listdir(datapath):
     fullpath = join(datapath, filename)
     print fullpath
@@ -40,10 +43,10 @@ for filename in listdir(datapath):
         print "Pushing %s ..." % filename
             
         (name, ext) = splitext(filename)
-        (type, month, year) = p.match(name).group(1,2,3)
-        tags = [{'name': type}, {'name': month}, {'name': year}]
+        (datatype, month, year) = p.match(name).group(1,2,3)
+        tags = [{'name': datatype}, {'name': month}, {'name': year}]
 
-        if type == 'EMS':
+        if datatype == 'EMS':
             csvreader = csv.reader(open(fullpath, 'rU'))
             for row in csvreader:
                 if len(row) > 2:
@@ -54,10 +57,12 @@ for filename in listdir(datapath):
                         tags.append({'name': subnet_description})
 
         try:
-            package = ckan.action.package_create(name=name.lower(), title=filename, owner_org='lancaster-university', tags=tags, asset_code='jeff')
+            if (datatype not in currentpackages):
+                package = ckan.action.package_create(datatype, title=datatype, owner_org='lancaster-university', tags=[{'name': datatype}], asset_code='')
+            #package = ckan.action.package_create(name=name.lower(), title=filename, owner_org='lancaster-university', tags=tags, asset_code='jeff')
         except Exception:
             print "Failed to create package %s" % name.lower()
-        try:
-            ckan.action.resource_create(package_id=package['id'], name=name.lower(), upload=open(fullpath))
-        except Exception:
-            print "Failed to create create resource %s" % name.lower()
+        #try:
+        #    ckan.action.resource_create(package_id=package['id'], name=name.lower(), upload=open(fullpath))
+        #except Exception:
+        #    print "Failed to create create resource %s" % name.lower()
